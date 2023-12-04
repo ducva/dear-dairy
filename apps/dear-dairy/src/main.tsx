c55155b73199849fc5b249fef69f30f2f54e432f
Michael <vuanhduc2685@gmail.com>
2023-12-04 22:35:37 +0700

    chore: add some libs

--- apps/dear-dairy/src/main.tsx
+++ apps/dear-dairy/src/main.tsx
@@ -1,14 +1,20 @@
 import * as ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react'
 import { BrowserRouter } from 'react-router-dom'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './app/app';
 import { AppConfig } from './configs'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
+// Create a client
+const queryClient = new QueryClient()
 const providerConfig = {
   domain: AppConfig.Auth0.Domain,
   clientId: AppConfig.Auth0.ClientId,
@@ -21,8 +27,11 @@ root.render(
   <Auth0Provider
     {...providerConfig}
   >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </Auth0Provider >
 );
